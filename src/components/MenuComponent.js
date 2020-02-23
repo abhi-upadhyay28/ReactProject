65d3ebc460f3243a9e54a464f91b110c2f6932c5
abhi-upadhyay28 <upadhyay28abhishek@gmail.com>
2020-02-23 13:11:55 +0530

    SecondWeek First Video

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -5,13 +5,8 @@ import  DishDetail from './DishDetailComponent';
 class Menu extends Component {
   constructor(props){
       super(props);
-      this.state={
-          selectedDish: null         
-            }
         }
-    onDishSelect(dish){
-        this.setState({selectedDish:dish});
-    }
+    
     renderDish(dish){
         if(dish!=null){
             return(<Card>
@@ -31,8 +26,7 @@ class Menu extends Component {
       const menu=this.props.dishes.map((dish) => {
           return(
               <div key={dish.id} className="col-12 col-md-5 m-1">
-                  <Card key={dish.id}
-                  onClick={()=>this.onDishSelect(dish)}>
+                  <Card onClick={()=>this.props.onClick(dish.id)}>
                       <CardImg width="100%" src={dish.image} alt={dish.name} />
                       <CardImgOverlay>
                         <CardTitle>{dish.name}</CardTitle>
@@ -48,9 +42,9 @@ class Menu extends Component {
             <div className="row">
                     {menu};
             </div>
-            <div className="row">
+            {/* <div className="row">
                     <DishDetail details={this.state.selectedDish} />
-            </div>
+            </div> */}
         </div>
       );
     }
