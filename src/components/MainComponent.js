46db3cb5d6850fd3829110fe7121d8064b5fcbd5
abhi-upadhyay28 <upadhyay28abhishek@gmail.com>
2020-03-09 02:17:52 +0530

    Single Page Applications part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -37,6 +37,13 @@ class Main extends React.Component {
         promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
         />
       );
+    };
+    const DishWithId = ({match}) =>{
+        return (
+          <DishDetail dish={this.state.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}
+          comments={this.state.comments.filter((comment) => comment.dishId===parseInt(match.params.dishId,10))}
+          />
+        );
     }
     return (
       <div className="App">
@@ -44,6 +51,7 @@ class Main extends React.Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
