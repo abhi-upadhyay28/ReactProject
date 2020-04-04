0a32292053532f51c2abc2b16131d34006f27267
abhi-upadhyay28 <upadhyay28abhishek@gmail.com>
2020-04-05 04:53:02 +0530

    React-Redux-Form

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,6 @@
 import {createStore} from 'redux';
 import {Reducer,initialState} from './reducer';
 
-export const ConfigureStore=()=>{
-    const store=createStore(
-        Reducer,
-        initialState
-    );
-}
+export const ConfigureStore = () => {
+    return createStore(Reducer,initialState);
+    };
