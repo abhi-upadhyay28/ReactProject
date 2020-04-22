d2a0f3995f5348eee310c89a32de40c951484beb
abhi-upadhyay28 <upadhyay28abhishek@gmail.com>
2020-04-23 02:45:23 +0530

    ReduxThunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import {Link} from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
     function RenderDish({dish}){
         if(dish!=null){
@@ -53,9 +54,30 @@ import {Link} from 'react-router-dom';
     }
   const DishDetail=(props)=>{
         //console.log('DishDetail Component render invoked');
-        const dish=props.dish;
-        if(dish==null)
+        if(props.isLoading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+
+            );
+        }
+        else if (props.errMess) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+
+            );
+        }
+        //const dish=props.dish;
+        else if(props.dish==null)
         return (<div></div>);
+        else{
         return(
             <div className="container">
                 <div className="row">
@@ -81,5 +103,6 @@ import {Link} from 'react-router-dom';
                 </div>
             </div>
       );
+    }
   }
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,7 +9,7 @@ import  Header from './HeaderComponent';
 import  Footer from './FooterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import {connect} from 'react-redux';
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 const mapStateToProps =state=>{
   return {
     dishes: state.dishes,
@@ -20,13 +20,18 @@ const mapStateToProps =state=>{
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)) 
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => {dispatch(fetchDishes())}
 });
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
     
+  }
+
+  componentDidMount(){
+    this.props.fetchDishes();
   }
 //   static defaultProps = { // <-- DEFAULT PROPS
 //     details: []       // undefined gets converted to array,render won't trigger error
@@ -35,7 +40,9 @@ class Main extends React.Component {
   render() {
     const HomePage = () => {
       return (
-        <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+        <Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+        dishesLoading={this.props.dishes.isLoading}
+        dishesErrMess={this.props.dishes.errMess}
         leader={this.props.leaders.filter((lead) => lead.featured)[0]}
         promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
         />
@@ -43,7 +50,9 @@ class Main extends React.Component {
     };
     const DishWithId = ({match}) =>{
         return (
-          <DishDetail dish={this.props.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}
+          <DishDetail dish={this.props.dishes.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
           comments={this.props.comments.filter((comment) => comment.dishId===parseInt(match.params.dishId,10))}
           addComment={this.props.addComment}
           />
