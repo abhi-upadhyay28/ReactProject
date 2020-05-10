a2b6649e61b3b5200b92e3ca70d7f361b8259915
abhi-upadhyay28 <upadhyay28abhishek@gmail.com>
2020-05-10 15:55:31 +0530

    LastWeekLastVideo

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -29,11 +29,7 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    
-  }
-
+  
   componentDidMount(){
     this.props.fetchDishes();
     this.props.fetchComments();
